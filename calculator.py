dcdee164858c4e72e7ebaa31e137079db483e09e
Ouafa6582 <wafa6582@gmail.com>
2025-08-13 18:28:16 +0100

    Create calculator.py
    إضافة كود آلة الحاسبة

---
+++ calculator.py
@@ -0,0 +1,9 @@
+print("welcome tp calculator app")
+
+num1=float(input("first number:"))
+num2=float(input("second number:"))
+print("the some  is:"+str(num1+num2))
+print("the multuply is:"+str(num1*num2))
+print("the divide is:"+str(num1/num2))
+print("the subtract is:"+str(num1-num2))
+print("the modulus is:"+str(num1%num2))
